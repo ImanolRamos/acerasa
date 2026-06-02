c9fe39aaa1b4ed5200a8be322d7197c445786401
Iñaki Cerezo <icerezo@koiote.es>
2026-06-02 14:20:46 +0200

    Arreglada gráfica

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,10 +3,16 @@ import App from './App.vue'
 
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
-import { createVuetify } from 'vuetify'
 
-const vuetify = createVuetify()
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
 createApp(App)
-    .use(vuetify)
-    .mount('#app')
+  .use(vuetify)
+  .mount('#app')
