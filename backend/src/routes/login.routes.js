82a1f54d58abdd8b3a323e52f6880bb273c41674
Iñaki Cerezo <icerezo@koiote.es>
2026-06-03 08:41:02 +0200

    arreglo back

--- backend/src/routes/login.routes.js
+++ backend/src/routes/login.routes.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const loginController = require('../controllers/login.controllers')
+const loginController = require('../controllers/login.controller')
 
 const router = express.Router()
 
